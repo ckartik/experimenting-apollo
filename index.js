dd7d6da8caa42ea8bf5e5b18417f3a9b45fb70cc
Kartik Chopra <k5chopra@uwaterloo.ca>
2021-05-20 21:51:03 -0400

    Adds posts microserviceg

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const { ApolloGateway} = require('@apollo/gateway');
 const port = 4000;
 
 const gateway = new ApolloGateway( {
-  serviceList: [{ name: "users", url: "http://localhost:4001"}],
+  serviceList: [
+    { name: "users", url: "http://localhost:4001"}, 
+    { name: "posts", url: "http://localhost:4002"},
+  ],
 });
 
 const server = new ApolloServer({
