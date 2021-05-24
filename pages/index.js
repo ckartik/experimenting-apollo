9b89b7cd15c10654c90729c617accc9a3bc2f0b7
Kartik Chopra <k5chopra@uwaterloo.ca>
2021-05-24 13:06:27 -0400

    Adds a front-end view

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
-import {gql, userQuery} from '@apollo/client'
+import {ApolloClient, gql, InMemoryCache, useQuery} from '@apollo/client'
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000",
+    cache: new InMemoryCache()
+})
+const getPosts = gql`
+    query fetchPost {
+        fetchAllPosts {
+            id
+            name,
+            author {
+                name
+            }
+        }
+    }
+`
+function Posts() {
+    const {loading, error, data} = useQuery(getPosts, {client:client});
+
+    if (loading) return 'Loading posts...';
+    if (error) return `Error! ${error.message}`;
+
+    return (
+        <ul>
+            {data.fetchAllPosts.map((post) => (
+                <li key={post.id}>
+                    <h4> {post.name} </h4>
+                    <p> Author: {post.author.name} </p>
+                    </li>
+            ))}
+        </ul>
+    );
+}
 
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div>
+        Welcome to Next.js!
+        <Posts/>
+        </div>
 }
   
 export default HomePage
